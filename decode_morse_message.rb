8866ad6aec72b3a3f28dc74207f5f327de1c6242
miguelesco <miuelangel1999@gmail.com>
2021-10-06 19:21:57 -0400

    fix single quotes errors

--- decode_morse_message.rb
+++ decode_morse_message.rb
@@ -1,67 +1,59 @@
-def decode(message = "")
+def decode(message = '')
   morse_code = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z",
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
   }
   message_length = message.length
-  decode_message = ""
-  code = ""
+  decode_message = ''
+  code = ''
   space = false
   counter = 0
-  message.each_char.with_index {
-    | char, index |
-    
-    if char == "-" || char == "."
+  message.each_char.with_index do |char, index|
+    if ['-', '.'].include?(char)
       code += char
       counter = 0
     end
 
-    if char == " " || char == '/'
+    if [' ', '/'].include?(char)
       counter += 1
-      if counter == 3
-        space = true
-      end
+      space = true if counter == 3
     end
 
-    if char == " " && morse_code[code] || index + 1 == message_length
+    if (char == ' ' && morse_code[code]) || index + 1 == message_length
       if space
-        decode_message += " "
+        decode_message += ' '
         space = false
         counter = 0
       end
       decode_message += morse_code[code]
-      code = ""
+      code = ''
     end
-
-
-  }
-  return decode_message
-
+  end
+  decode_message
 end
 
-puts decode(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
-
+puts decode(' .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
